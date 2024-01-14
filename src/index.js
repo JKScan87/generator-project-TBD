d5a574da82f73004f5495dd32356a833d4fe5c47
JKScan87 <jill.scanlon@gmail.com>
2024-01-14 13:01:33 -0800

    Added basic form handling and typewriter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "Two roads diverged in a yellow wood",
+    autoStart: true,
+    delay: 1,
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
